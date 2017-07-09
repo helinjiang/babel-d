c7d9f2a8a0a5b065791e30ec7c940ba656d3327d
helinjiang <me@helinjiang.com>
2017-07-09 22:22:03 +0800

    feat: add test

---
+++ test/data/expected/es6-export.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.__esModule = true;
+exports.default = sayHello;
+function sayHello(name) {
+    console.log('Hello2, ' + name + '!');
+}
+
+sayHello('world');
---
+++ test/data/fixtures/es6-export.js
@@ -0,0 +1,5 @@
+export default function sayHello(name) {
+    console.log(`Hello2, ${name}!`);
+}
+
+sayHello('world');
