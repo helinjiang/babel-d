22a227e6c4f48a1d49f22e2fb972579be655d6e9
linjianghe <linjianghe@tencent.com>
2018-05-11 20:39:32 +0800

    feat: 增加更多测试用例的测试场景

--- test/data/expected/es6-export.js
+++ test/data/expected/es6-export.js
@@ -1,11 +1,34 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 exports.default = sayHello;
 function sayHello(name) {
-    console.log('Hello, ' + name + '!');
+
+  var target = {
+    age: 12,
+    desc: 'i like desc'
+  };
+
+  var result = _extends({}, target);
+
+  result.tName = name;
+
+  var _result = result,
+      tName = _result.tName;
+
+
+  var wording = 'Hello, ' + tName + '!';
+
+  result = Object.assign({}, result, {
+    wording: wording
+  });
+
+  console.log('Hello, ' + name + '!', result);
 }
 
 sayHello('world');
